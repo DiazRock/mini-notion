fbbbb83622af7f3c3c58cbbff1edfc77e54f7eff
DiazRock <corolariodiaz@gmail.com>
2024-12-12 00:56:28 +0200

    Added controllers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from .database import engine, Base, SessionLocal
-from .routes import users, tasks, notes
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .dep_container import engine, Base
+from .controllers import users, tasks, notes, auth
 
-app = FastAPI()
+
+
+app = FastAPI(title="Notion API",
+    description="This API provides the functionality for the mini-notion app.",
+    version="1.0.0",
+    contact={
+        "name": "Alejandro Diaz Roque",
+        "url": "https://github.com/DiazRock",
+        "email": "corolariodiaz@gmail.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    }
+)
 
 Base.metadata.create_all(bind=engine)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3005"],  # Specify the exact origin here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.users_router)
 app.include_router(tasks.tasks_router)
 app.include_router(notes.notes_router)
+app.include_router(auth.auth_router)
+
+
+
